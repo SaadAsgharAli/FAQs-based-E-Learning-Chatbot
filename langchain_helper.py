3c4ad2db71d3845db2d390a3fdbdcb04afb87147
unknown <saadasghar209@gmail.com>
2024-09-27 11:43:19 +0500

    CHATBOT

---
+++ langchain_helper.py
@@ -0,0 +1,71 @@
+from langchain.vectorstores import FAISS
+#from langchain.llms import GooglePalm
+from langchain_google_genai import ChatGoogleGenerativeAI  # by me
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+import os
+
+api_key = os.getenv('API_KEY')
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash",
+    temperature=0.2,
+    max_output_tokens=8192,
+    timeout=None,
+    max_retries=2,
+    top_p= None,   # you can try 0.95
+    top_k= None,    # you can try 64
+    # other params...
+    # https://api.python.langchain.com/en/latest/chat_models/langchain_google_genai.chat_models.ChatGoogleGenerativeAI.html
+)
+# # Initialize instructor embeddings using the Hugging Face model
+instructor_embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large")
+vectordb_file_path = "faiss_index"
+
+def create_vector_db():
+    # Load data from FAQ sheet
+    loader = CSVLoader(file_path='faqs.csv', encoding="ISO-8859-1", source_column="prompt")
+    data = loader.load()
+
+    # Create a FAISS instance for vector database from 'data'
+    vectordb = FAISS.from_documents(documents=data,
+                                    embedding=instructor_embeddings)
+
+    # Save vector database locally
+    vectordb.save_local(vectordb_file_path)
+
+
+def get_qa_chain():
+    # Load the vector database from the local folder
+    vectordb = FAISS.load_local(vectordb_file_path, instructor_embeddings, allow_dangerous_deserialization=True)
+
+    # Create a retriever for querying the vector database
+    retriever = vectordb.as_retriever(score_threshold=0.7)
+
+    prompt_template = """Given the following context and a question, generate an answer based on this context only.
+    In the answer try to provide as much text as possible from "response" section in the source document context without making much changes.
+    If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer.
+
+    CONTEXT: {context}
+
+    QUESTION: {question}"""
+
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+
+    chain = RetrievalQA.from_chain_type(llm=llm,
+                                        chain_type="stuff",
+                                        retriever=retriever,
+                                        input_key="query",
+                                        return_source_documents=True,
+                                        chain_type_kwargs={"prompt": PROMPT})
+
+    return chain
+
+if __name__ == "__main__":
+    create_vector_db()
+    chain = get_qa_chain()
+    print(chain("Do you have javascript course?"))
